cfea227304163babfa697b717f218ee4dd725ce6
surajmac <surajnair.mac@gmail.com>
2022-03-10 18:31:40 -0500

    Enhancement4 added ability to view author details page

--- authors/models.py
+++ authors/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 
 # Create your models here.
+from django.urls import reverse
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     pseudonym = models.CharField(max_length=200)
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Author
+from  articles.models import Article
+
+
+# Create your views here.
+class AuthorsDetailView(generic.DetailView):
+    model = Author
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(AuthorsDetailView, self).get_context_data()
+
+        totals = dict()
+        query_set = Article.objects.filter(author_id__exact=self.kwargs['pk']).select_related('genre')
+        for article in query_set:
+            if article.genre in totals.keys():
+                totals[article.genre] += 1
+            else:
+                totals[article.genre] = 1
+
+        context['totals'] = totals
+
+        return context
+
 
 # Create your views here.
