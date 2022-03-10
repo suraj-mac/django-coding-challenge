4f8e3b5fe723da90268e38fa165dd347a7306f5c
surajmac <surajnair.mac@gmail.com>
2022-03-09 20:31:41 -0500

    BugFix-1 modified query by including foreign key to improve loading performance

--- articles/views.py
+++ articles/views.py
@@ -2,16 +2,16 @@ from django.views import generic
 from .models import Article
 
 
+
 # Create your views here.
 class ArticleDetailView(generic.DetailView):
     model = Article
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ArticleDetailView, self).get_context_data()
-
         totals = dict()
-
-        for article in Article.objects.exclude(published=None):
+        query_set = Article.objects.exclude(published=None).select_related('genre', 'author')
+        for article in query_set:
             if article.genre in totals.keys():
                 totals[article.genre] += 1
             else:
@@ -24,20 +24,21 @@ class ArticleDetailView(generic.DetailView):
 
 class ArticleListView(generic.ListView):
     model = Article
+    queryset = Article.objects.exclude(published=None).select_related('genre', 'author')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ArticleListView, self).get_context_data()
-
         totals = dict()
 
-        for article in Article.objects.exclude(published=None):
+        query_set = Article.objects.exclude(published=None).select_related('genre', 'author')
+        for article in query_set:
             if article.genre in totals.keys():
                 totals[article.genre] += 1
             else:
                 totals[article.genre] = 1
 
         context['totals'] = totals
-
+        print("stopped")
         return context
 
 
@@ -45,13 +46,14 @@ class ArticleWeekView(generic.WeekArchiveView):
     model = Article
     date_field = 'published'
     allow_future = False
+    make_object_list = True
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ArticleWeekView, self).get_context_data()
 
         totals = dict()
-
-        for article in Article.objects.exclude(published=None):
+        query_set = Article.objects.exclude(published=None).select_related('genre', 'author')
+        for article in query_set:
             if article.genre in totals.keys():
                 totals[article.genre] += 1
             else:
@@ -66,13 +68,14 @@ class ArticleMonthView(generic.MonthArchiveView):
     model = Article
     date_field = 'published'
     allow_future = False
+    make_object_list = True
+    month_format = '%m'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ArticleMonthView, self).get_context_data()
-
         totals = dict()
-
-        for article in Article.objects.exclude(published=None):
+        query_set = Article.objects.exclude(published=None).select_related('genre', 'author')
+        for article in query_set:
             if article.genre in totals.keys():
                 totals[article.genre] += 1
             else:
@@ -87,13 +90,13 @@ class ArticleYearView(generic.YearArchiveView):
     model = Article
     date_field = 'published'
     allow_future = False
+    make_object_list = True
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ArticleYearView, self).get_context_data()
-
         totals = dict()
-
-        for article in Article.objects.exclude(published=None):
+        query_set = Article.objects.exclude(published=None).select_related('genre', 'author')
+        for article in query_set:
             if article.genre in totals.keys():
                 totals[article.genre] += 1
             else:
