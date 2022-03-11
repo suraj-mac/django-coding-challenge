f9b75b72a5d0b1e6f98a7f33fec1654ec8f39bf5
surajmac <surajnair.mac@gmail.com>
2022-03-10 21:32:13 -0500

    Enhancement5 Refactored articles/views.py to DRY principle

--- articles/views.py
+++ articles/views.py
@@ -1,24 +1,26 @@
+from django.db.models import Count, QuerySet
 from django.views import generic
-from .models import Article
+from .models import Article, Genre
 
+
+def aggreate_by_genre_field(query_set: QuerySet) -> {}:
+    totals = dict()
+    for article in query_set:
+        if article.genre in totals.keys():
+            totals[article.genre] += 1
+        else:
+            totals[article.genre] = 1
+    return totals
 
 
 # Create your views here.
 class ArticleDetailView(generic.DetailView):
     model = Article
+    queryset = Article.objects.exclude(published=None).select_related('genre', 'author')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ArticleDetailView, self).get_context_data()
-        totals = dict()
-        query_set = Article.objects.exclude(published=None).select_related('genre', 'author')
-        for article in query_set:
-            if article.genre in totals.keys():
-                totals[article.genre] += 1
-            else:
-                totals[article.genre] = 1
-
-        context['totals'] = totals
-
+        context['totals'] = aggreate_by_genre_field(self.queryset)
         return context
 
 
@@ -28,17 +30,7 @@ class ArticleListView(generic.ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ArticleListView, self).get_context_data()
-        totals = dict()
-
-        query_set = Article.objects.exclude(published=None).select_related('genre', 'author')
-        for article in query_set:
-            if article.genre in totals.keys():
-                totals[article.genre] += 1
-            else:
-                totals[article.genre] = 1
-
-        context['totals'] = totals
-        print("stopped")
+        context['totals'] = aggreate_by_genre_field(self.queryset)
         return context
 
 
@@ -47,20 +39,11 @@ class ArticleWeekView(generic.WeekArchiveView):
     date_field = 'published'
     allow_future = False
     make_object_list = True
+    queryset = Article.objects.exclude(published=None).select_related('genre', 'author')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ArticleWeekView, self).get_context_data()
-
-        totals = dict()
-        query_set = Article.objects.exclude(published=None).select_related('genre', 'author')
-        for article in query_set:
-            if article.genre in totals.keys():
-                totals[article.genre] += 1
-            else:
-                totals[article.genre] = 1
-
-        context['totals'] = totals
-
+        context['totals'] = aggreate_by_genre_field(self.queryset)
         return context
 
 
@@ -70,19 +53,11 @@ class ArticleMonthView(generic.MonthArchiveView):
     allow_future = False
     make_object_list = True
     month_format = '%m'
+    queryset = Article.objects.exclude(published=None).select_related('genre', 'author')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ArticleMonthView, self).get_context_data()
-        totals = dict()
-        query_set = Article.objects.exclude(published=None).select_related('genre', 'author')
-        for article in query_set:
-            if article.genre in totals.keys():
-                totals[article.genre] += 1
-            else:
-                totals[article.genre] = 1
-
-        context['totals'] = totals
-
+        context['totals'] = aggreate_by_genre_field(self.queryset)
         return context
 
 
@@ -91,17 +66,9 @@ class ArticleYearView(generic.YearArchiveView):
     date_field = 'published'
     allow_future = False
     make_object_list = True
+    queryset = Article.objects.exclude(published=None).select_related('genre', 'author')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ArticleYearView, self).get_context_data()
-        totals = dict()
-        query_set = Article.objects.exclude(published=None).select_related('genre', 'author')
-        for article in query_set:
-            if article.genre in totals.keys():
-                totals[article.genre] += 1
-            else:
-                totals[article.genre] = 1
-
-        context['totals'] = totals
-
+        context['totals'] = aggreate_by_genre_field(self.queryset)
         return context
