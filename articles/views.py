00ad91d2a1a91cb56f457d60bd2dcfdae6705c7d
surajmac <surajnair.mac@gmail.com>
2022-03-13 20:29:06 -0400

    Refactoring common code

--- articles/views.py
+++ articles/views.py
@@ -1,17 +1,8 @@
 from django.db.models import Count, QuerySet
 from django.views import generic
+
+from services.articleService import aggreate_by_genre_field
 from .models import Article, Genre
-
-
-def aggreate_by_genre_field(query_set: QuerySet) -> {}:
-    totals = dict()
-    for article in query_set:
-        if article.genre in totals.keys():
-            totals[article.genre] += 1
-        else:
-            totals[article.genre] = 1
-    return totals
-
 
 # Create your views here.
 class ArticleDetailView(generic.DetailView):
--- authors/views.py
+++ authors/views.py
@@ -1,26 +1,14 @@
 from django.views import generic
+
+from services.articleService import aggreate_by_genre_field
 from .models import Author
 from  articles.models import Article
 
-
-# Create your views here.
 class AuthorsDetailView(generic.DetailView):
     model = Author
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(AuthorsDetailView, self).get_context_data()
-
-        totals = dict()
-        query_set = Article.objects.filter(author_id__exact=self.kwargs['pk']).select_related('genre')
-        for article in query_set:
-            if article.genre in totals.keys():
-                totals[article.genre] += 1
-            else:
-                totals[article.genre] = 1
-
-        context['totals'] = totals
-
+        queryset = Article.objects.filter(author_id__exact=self.kwargs['pk']).select_related('genre')
+        context['totals'] = aggreate_by_genre_field(queryset)
         return context
-
-
-# Create your views here.
